9c1a0fb57eca1dadb5b6adad4d5dd5703f0ad3d8
Anshul Rai <anshulrai02092003@gmail.com>
2025-02-10 22:19:18 +0530

    [Anshul] Add dynamicmarketplace package for Dynamic Online Marketplace project

---
+++ Day1-Java-Generics/src/test/java/dynamicmarketplace/BookCategoryTest.java
@@ -0,0 +1,13 @@
+package dynamicmarketplace;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookCategoryTest {
+
+    @Test
+    void testBookCategory() {
+        BookCategory category = new BookCategory("Fiction");
+        assertEquals("Fiction", category.getGenre());
+    }
+}
---
+++ Day1-Java-Generics/src/test/java/dynamicmarketplace/ClothingCategoryTest.java
@@ -0,0 +1,13 @@
+package dynamicmarketplace;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClothingCategoryTest {
+
+    @Test
+    void testClothingCategory() {
+        ClothingCategory category = new ClothingCategory("Casual");
+        assertEquals("Casual", category.getType());
+    }
+}
