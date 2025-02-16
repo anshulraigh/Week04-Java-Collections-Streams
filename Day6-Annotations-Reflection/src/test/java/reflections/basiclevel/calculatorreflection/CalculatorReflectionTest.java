51381d97ef3f61efec12f2356109ed6964993ab9
Anshul Rai <anshulrai02092003@gmail.com>
2025-02-16 10:38:57 +0530

    [Anshul] Add. calculator class with reflection, and it's test

---
+++ Day6-Annotations-Reflection/src/test/java/reflections/basiclevel/calculatorreflection/CalculatorReflectionTest.java
@@ -0,0 +1,28 @@
+package reflections.basiclevel.calculatorreflection;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
+class CalculatorReflectionTest {
+
+    @Test
+    void testInvokePrivateMethod() {
+        try {
+
+            Calculator calculator = new Calculator();
+
+
+            Method multiplyMethod = Calculator.class.getDeclaredMethod("multiply", int.class, int.class);
+            multiplyMethod.setAccessible(true);
+
+
+            int result = (int) multiplyMethod.invoke(calculator, 6, 3);
+            assertEquals(18, result, "Multiplication should return 18 for inputs (6,3)");
+
+        } catch (Exception e) {
+            fail("Exception occurred: " + e.getMessage());
+        }
+    }
+}
