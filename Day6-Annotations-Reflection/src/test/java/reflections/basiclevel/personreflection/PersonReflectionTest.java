6ce61b82c458465bc9ef2637c6b54a1eb60434cd
Anshul Rai <anshulrai02092003@gmail.com>
2025-02-16 10:39:48 +0530

    [Anshul] Add. person class with reflection, and it's test

---
+++ Day6-Annotations-Reflection/src/test/java/reflections/basiclevel/personreflection/PersonReflectionTest.java
@@ -0,0 +1,26 @@
+package reflections.basiclevel.personreflection;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
+class PersonReflectionTest {
+
+    @Test
+    void testModifyPrivateField() {
+        try {
+            Person person = new Person(25);
+
+
+            Field ageField = Person.class.getDeclaredField("age");
+            ageField.setAccessible(true);
+
+            ageField.set(person, 30);
+
+            assertEquals(30, person.getAge(), "The age field should be updated to 30.");
+
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            fail("Exception occurred: " + e.getMessage());
+        }
+    }
+}
