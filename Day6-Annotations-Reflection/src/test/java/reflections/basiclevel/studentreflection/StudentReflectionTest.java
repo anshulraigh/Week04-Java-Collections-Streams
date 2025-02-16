75484eca3c443807c7ee0bd4301de6b3d6fbb277
Anshul Rai <anshulrai02092003@gmail.com>
2025-02-16 10:40:36 +0530

    [Anshul] Add. student class with reflection, and it's test

---
+++ Day6-Annotations-Reflection/src/test/java/reflections/basiclevel/studentreflection/StudentReflectionTest.java
@@ -0,0 +1,29 @@
+package reflections.basiclevel.studentreflection;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Constructor;
+
+class StudentReflectionTest {
+
+    @Test
+    void testCreateStudentInstance() {
+        try {
+
+            Class<?> studentClass = Student.class;
+
+
+            Constructor<?> constructor = studentClass.getDeclaredConstructor();
+
+
+            Object studentInstance = constructor.newInstance();
+
+
+            assertNotNull(studentInstance, "Student instance should not be null.");
+            assertTrue(studentInstance instanceof Student, "Created object should be an instance of Student.");
+
+        } catch (Exception e) {
+            fail("Exception occurred: " + e.getMessage());
+        }
+    }
+}
